36d3790cf7eb49eb5d82d387f6d3da038ff3369e
skymountain <ryukilon@gmail.com>
2013-03-17 01:46:23 +0900

    add wish_book cgi

---
+++ api/wish_book.ml
@@ -0,0 +1,12 @@
+open BibmanNet
+;;
+
+let main (cgi: Netcgi.cgi) : unit =
+  ignore (redirect_to_script cgi "../script/catalog" [ "wish_book"; ])
+;;
+
+let () = run
+  ~req_http_method:[`GET; `HEAD; ]
+  ~req_content_type:[ MimeType.json; ]
+  main
+;;
